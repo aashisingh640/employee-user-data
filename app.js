63347ae6ca0e9a73a309d480c3a1864e918e0605
INDCORP\703224802 <aashi.singh@genpact.digital>
2022-03-16 16:51:24 +0530

    initial project

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const path = require('path');
 
 const userRoute = require('./routes/userRoute');
 
 const app = express();
 
 app.use(express.json());
-// app.use(express.static(`${__dirname}/public`));
+app.use('/', express.static(path.join(__dirname,'/dist/users-project')));
 
 app.use('/api/users', userRoute);
 
