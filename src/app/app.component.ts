cde730abf181e967ff612948271e896052b24760
INDCORP\703224802 <aashi.singh@genpact.digital>
2022-03-16 12:18:35 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SharedService } from './services/shared.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'UsersProject';
+
+  /**contains the row data */
+  rows: Array<any> = [];
+  
+  /**whether any row contains invalid data */
+  invalidData: boolean = false;
+
+  constructor(private shared: SharedService) { }
+
+  ngOnInit() {
+    this.getData();
+  }
+
+  /**fetch the records from the server */
+  getData() {
+    this.shared.getAllUsers().subscribe(
+      response => {
+        this.rows = response.data.users;
+        this.invalidData = response.data.invalid;
+      }, error => {
+        console.log(error);
+      }
+    )
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { AgGridModule } from 'ag-grid-angular';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { InvalidUserComponent } from './components/invalid-user/invalid-user.component';
+import { TableComponent } from './components/table/table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InvalidUserComponent,
+    TableComponent,
+    InvalidUserComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AgGridModule.withComponents([
+      InvalidUserComponent
+    ]),
     AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
